650ad35913bd751916fef513ddb5afbded7884b1
Momen Sherif <momensherif.2019@gmail.com>
2021-09-23 15:30:56 +0200

    📦 NEW: useCounter

--- libs/react-hooks-extended/src/lib/use-counter/use-counter.spec.ts
+++ libs/react-hooks-extended/src/lib/use-counter/use-counter.spec.ts
@@ -3,14 +3,11 @@ import useCounter from './use-counter';
 
 describe('useCounter', () => {
   it('should render successfully', () => {
-    const { result } = renderHook(() => useCounter());
-
+    const { result } = renderHook(() => useCounter(0, { step: 2 }));
     expect(result.current.count).toBe(0);
-
     act(() => {
       result.current.increment();
     });
-
-    expect(result.current.count).toBe(1);
+    expect(result.current.count).toBe(2);
   });
 });
--- libs/react-hooks-extended/src/lib/use-counter/use-counter.ts
+++ libs/react-hooks-extended/src/lib/use-counter/use-counter.ts
@@ -1,14 +1,79 @@
 import { useState, useCallback } from 'react';
 
-export interface UseCounter {
-  count: number;
-  increment: () => void;
+export type InitialCount = number | (() => number);
+export interface UseCounterOptions {
+  /**
+   * Minimum counter value
+   * @default Number.MIN_SAFE_INTEGER
+   */
+  min?: number;
+  /**
+   * Maximum counter value
+   * @default Number.MAX_SAFE_INTEGER
+   */
+  max?: number;
+  /**
+   * Counter increment step
+   * @default 1
+   */
+  step?: number;
 }
 
-export function useCounter(): UseCounter {
-  const [count, setCount] = useState(0);
-  const increment = useCallback(() => setCount(x => x + 1), []);
-  return { count, increment };
+// export interface UseCounter {}
+
+/**
+ * @param initialCount - initial counter
+ * @param options - counter options
+ * @default initialCount 0
+ */
+export function useCounter(
+  initialCount: InitialCount = 0,
+  options: UseCounterOptions = {}
+) {
+  const {
+    step = 1,
+    min = Number.MIN_SAFE_INTEGER,
+    max = Number.MAX_SAFE_INTEGER,
+  } = options;
+
+  const [count, setCount] = useState(initialCount);
+
+  const increment = useCallback(
+    () => setCount(c => Math.min(c + step, max)),
+    [max, step]
+  );
+
+  const decrement = useCallback(
+    () => setCount(c => Math.max(c - step, min)),
+    [min, step]
+  );
+
+  const incrementBy = useCallback(
+    (value: number) => setCount(c => Math.min(c + value, max)),
+    [max]
+  );
+
+  const decrementBy = useCallback(
+    (value: number) => setCount(c => Math.max(c - value, min)),
+    [min]
+  );
+
+  const set = useCallback(
+    (value: number) => setCount(value > max ? max : value < min ? min : value),
+    [max, min]
+  );
+
+  const reset = useCallback(() => setCount(initialCount), [initialCount]);
+
+  return {
+    count,
+    increment,
+    decrement,
+    incrementBy,
+    decrementBy,
+    set,
+    reset,
+  } as const;
 }
 
 export default useCounter;
