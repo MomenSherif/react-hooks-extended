5b00e07c6eed87b033d8243720e8c53148c45e8e
Momen Sherif <momensherif.2019@gmail.com>
2021-10-28 17:41:03 +0200

    📖 DOC: useArrayState

--- libs/react-hooks-extended/src/lib/use-array-state/use-array-state.ts
+++ libs/react-hooks-extended/src/lib/use-array-state/use-array-state.ts
@@ -1,32 +1,32 @@
 import { useState, useCallback, Dispatch, SetStateAction } from 'react';
 
-export interface UseArrayStateUpdaters<T> {
+export interface UseArrayStateUpdaters<S> {
   /** Set new array state */
-  set: Dispatch<SetStateAction<T[]>>;
+  set: Dispatch<SetStateAction<S[]>>;
   /** Adds one or more elements to the end of an array */
-  push(...value: T[]): void;
+  push(...value: S[]): void;
   /** Adds one or more elements to the beginning  of an array */
-  unshift(...value: T[]): void;
+  unshift(...value: S[]): void;
   /** Removes the last element from an array */
   pop(): void;
   /** Removes the first element from an array */
   shift(): void;
   /** Removes all elements that pass the test implemented by the predicate function */
-  remove(predicate: (value: T, index: number, array: T[]) => boolean): void;
+  remove(predicate: (value: S, index: number, array: S[]) => boolean): void;
   /**
    * Update all elements that pass the test implemented by the predicate function.
    * @param predicate - Should return true for elements that need to be updated
    * @param updater - A function takes previous value & index for updated item and returns updated item
    */
   update(
-    predicate: (value: T, index: number, array: T[]) => boolean,
-    updater: (prevValue: T, index: number) => T
+    predicate: (value: S, index: number, array: S[]) => boolean,
+    updater: (prevValue: S, index: number) => S
   ): void;
   /** Removes all array elements */
   removeAll(): void;
 }
 
-export type UseArrayState<T> = [T[], UseArrayStateUpdaters<T>];
+export type UseArrayState<S> = [S[], UseArrayStateUpdaters<S>];
 
 /**
  * Manage state in array format.
@@ -36,46 +36,46 @@ export type UseArrayState<T> = [T[], UseArrayStateUpdaters<T>];
  *  const [state, { push, unshift, pop, shift, remove, removeAll }] = useArrayState([]);
  * @returns Returns a stateful value and methods to update the array.
  */
-export function useArrayState<T>(
-  initialState: T[] | (() => T[])
-): UseArrayState<T> {
+export function useArrayState<S>(
+  initialState: S[] | (() => S[])
+): UseArrayState<S> {
   const [state, setState] = useState(() => {
     return initialState instanceof Function ? initialState() : initialState;
   });
 
-  const push: UseArrayStateUpdaters<T>['push'] = useCallback((...values) => {
+  const push: UseArrayStateUpdaters<S>['push'] = useCallback((...values) => {
     setState(prevState => [...prevState, ...values]);
   }, []);
 
-  const unshift: UseArrayStateUpdaters<T>['unshift'] = useCallback(
+  const unshift: UseArrayStateUpdaters<S>['unshift'] = useCallback(
     (...values) => {
       setState(prevState => [...values, ...prevState]);
     },
     []
   );
 
-  const pop: UseArrayStateUpdaters<T>['pop'] = useCallback(() => {
+  const pop: UseArrayStateUpdaters<S>['pop'] = useCallback(() => {
     setState(prevState => {
       if (!prevState.length) return prevState;
       return prevState.slice(0, prevState.length - 1);
     });
   }, []);
 
-  const shift: UseArrayStateUpdaters<T>['shift'] = useCallback(() => {
+  const shift: UseArrayStateUpdaters<S>['shift'] = useCallback(() => {
     setState(prevState => {
       if (!prevState.length) return prevState;
       return prevState.slice(1);
     });
   }, []);
 
-  const remove: UseArrayStateUpdaters<T>['remove'] = useCallback(predicate => {
+  const remove: UseArrayStateUpdaters<S>['remove'] = useCallback(predicate => {
     setState(prevState => {
       if (!prevState.length) return prevState;
       return prevState.filter((...args) => !predicate(...args));
     });
   }, []);
 
-  const update: UseArrayStateUpdaters<T>['update'] = useCallback(
+  const update: UseArrayStateUpdaters<S>['update'] = useCallback(
     (predicate, updater) => {
       setState(prevState =>
         prevState.map((v, i, a) => (predicate(v, i, a) ? updater(v, i) : v))
@@ -84,7 +84,7 @@ export function useArrayState<T>(
     []
   );
 
-  const removeAll: UseArrayStateUpdaters<T>['removeAll'] = useCallback(() => {
+  const removeAll: UseArrayStateUpdaters<S>['removeAll'] = useCallback(() => {
     setState(prevState => {
       if (!prevState.length) return prevState;
       return [];
